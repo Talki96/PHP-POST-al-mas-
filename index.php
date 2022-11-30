902b5966972778e10e876865aa2ee8de4160d11c
Talki96 <115979143+Talki96@users.noreply.github.com>
2022-11-30 13:41:24 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POST</title>
+</head>
+<body>
+
+<form action="" method="post">
+    <label for="number">Sayı girin</label>
+    <input type="number" name="number" placeholder="Sayı girin...">
+    <button type="submit">Gönder</button>
+</form>
+
+</body>
+</html>
